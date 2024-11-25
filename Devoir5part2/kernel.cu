f4b48cfcc48de41156a5fe1764de98c8d204fbeb
Lacommande <Pietro.Lacommande@uqtr.ca>
2024-11-25 00:50:00 -0500

    Added part2 and reorganized files

---
+++ Devoir5part2/kernel.cu
@@ -0,0 +1,169 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <cstdlib> // For rand()
+#include <ctime>   // For seeding rand()
+
+cudaError_t multiplyWithCuda(float* a, const float* b, const float* c, unsigned int size);
+
+void printMatrix(const float* matrix, int rows, int columns, const char* name) {
+    printf("Matrix %s:\n", name);
+    for (int i = 0; i < rows; ++i) {
+        printf("{ "); // Start of row delimiter
+        for (int j = 0; j < columns; ++j) {
+            printf("%.2f", matrix[i * columns + j]); // Print with 2 decimal place
+            if (j < columns - 1) {
+                printf(" "); // Add space between elements
+            }
+        }
+        printf(" }"); // End of row delimiter
+        printf("\n"); // Newline after each row
+    }
+    printf("\n");
+}
+
+
+
+float* createArray(int rows, int columns) {
+    int totalSize = rows * columns;
+
+    float* array = new float[totalSize];
+
+    for (int i = 0; i < totalSize; ++i) {
+        array[i] = static_cast<float>(std::rand()) / RAND_MAX * 4.0f; // Random floats [0, 4]    
+    }
+    return array;
+}
+
+
+__global__ void multiplyKernel(float* a, const float* b, const float* c)
+{
+    int row = (blockIdx.y * blockDim.y) + threadIdx.y;
+    int linearIndex;
+    float sum = 0.0;
+    
+
+    for (int index=0; index < 64 && row <64; index++) {
+        linearIndex = (row * 64) + index;
+        sum += b[linearIndex] * c[index];
+    }
+    a[row] = sum;
+}
+
+
+int main()
+{
+    const int arraySize = 64;
+    const float* b = createArray(64, 64);
+    const float* c = createArray(64, 1);
+    float a[arraySize] = { 0 };
+
+    // Print matrices A and B before computation
+    printMatrix(b, 64, 64, "B");
+    printMatrix(c, 64, 1, "C");
+
+    // Add vectors in parallel.
+    cudaError_t cudaStatus = multiplyWithCuda(a, b, c, arraySize);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addWithCuda failed!");
+        return 1;
+    }
+
+    // Print result matrix C after computation
+    printMatrix(a, 64, 1, "A");
+
+    // cudaDeviceReset must be called before exiting in order for profiling and
+    // tracing tools such as Nsight and Visual Profiler to show complete traces.
+    cudaStatus = cudaDeviceReset();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceReset failed!");
+        return 1;
+    }
+
+    return 0;
+}
+
+// Helper function for using CUDA to add vectors in parallel.
+cudaError_t multiplyWithCuda(float* a, const float* b, const float* c, unsigned int size)
+{
+    float* dev_a = 0;
+    float* dev_b = 0;
+    float* dev_c = 0;
+    cudaError_t cudaStatus;
+
+    // Choose which GPU to run on, change this on a multi-GPU system.
+    cudaStatus = cudaSetDevice(0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+        goto Error;
+    }
+
+    // Allocate GPU buffers for three vectors (two input, one output)    .
+    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMalloc((void**)&dev_b, (size*size) * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    // Copy input vectors from host memory to GPU buffers.
+    cudaStatus = cudaMemcpy(dev_b, b, (size*size)* sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMemcpy(dev_c, c, size * sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+    // Launch a kernel on the GPU with one thread for each element.
+    // Cette config de grid, blocks, threads représente parfaitement une matrice de 64x64 tout en ayant un nombre de threads par block optimal
+    dim3 threadsParBlock(16, 16, 1);
+    dim3 nombreDeBlock(4, 4, 1);
+    multiplyKernel << <nombreDeBlock, threadsParBlock >> > (dev_a, dev_b, dev_c);
+
+    // Check for any errors launching the kernel
+    cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        goto Error;
+    }
+
+    // cudaDeviceSynchronize waits for the kernel to finish, and returns
+    // any errors encountered during the launch.
+    cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+        goto Error;
+    }
+
+    // Copy output vector from GPU buffer to host memory.
+    cudaStatus = cudaMemcpy(a, dev_a, size * sizeof(int), cudaMemcpyDeviceToHost);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+Error:
+    cudaFree(dev_c);
+    cudaFree(dev_a);
+    cudaFree(dev_b);
+
+    return cudaStatus;
+}
